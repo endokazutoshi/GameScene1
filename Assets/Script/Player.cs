2006c20874a200b492ccb8a7a6c82d0bca61bb01
JC-21\220410 <kazu062ek@gmail.com>
2024-05-08 13:50:41 +0900

    プレイヤーの斜め移動無し

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float _speed = 5.0f;       //プレイヤーの移動スピード
+    [SerializeField] private float _speed;       //プレイヤーの移動スピード
     private float distance = 1.0f;　　　//プレイヤーの移動距離
     private Vector2 move;
     private Vector2 targetPos; // Vector2に変更
@@ -17,8 +17,37 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        move.x = Input.GetAxisRaw("Horizontal");
-        move.y = Input.GetAxisRaw("Vertical");
+        Vector2 move = Vector2.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            move.y = 1;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            move.y = -1;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            move.x = -1;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            move.x = 1;
+        }
+
+        if (move.x != 0 && move.y != 0)
+        {
+            if (Mathf.Abs(move.x) > Mathf.Abs(move.y))
+            {
+                move.y = 0; // 上下方向の移動を無効化
+            }
+            else
+            {
+                move.x = 0; // 左右方向の移動を無効化
+            }
+        }
+
         if (move != Vector2.zero && (Vector2)transform.position == targetPos) // transform.positionをVector2にキャストして比較
         {
             targetPos += move * distance; // Vector2同士の加算に変更
