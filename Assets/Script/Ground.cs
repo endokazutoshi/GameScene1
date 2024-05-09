72c7b12f3572f4f2d9a149350861a0fd127ace60
JC-21\220410 <kazu062ek@gmail.com>
2024-05-09 14:08:42 +0900

    プレイシーンを二分割にした。

--- Assets/Script/Ground.cs
+++ Assets/Script/Ground.cs
@@ -10,17 +10,22 @@ public class Ground : MonoBehaviour
     {
         for (int length = 0; length < _length; length++)
         {
-            for (int width= 0; width < _width; width++)
+            for (int width = 0; width < _width; width++)
             {
-                if (length >= 1 && length < 23) // ‰¡‚ÌÀ•W‚ª0‚©‚ç22‚Ì”ÍˆÍ‚Ìê‡
+                if (length >= 1 && length < 22 && width >= 1 && width < 9) // ‰¡‚ÌÀ•W‚ª0‚©‚ç22‚Ì”ÍˆÍ‚Ìê‡
                 {
                     Instantiate(_wallPrefab, new Vector3(length, width, 0), Quaternion.identity, transform);
                 }
                 else // ‚»‚êˆÈŠO‚Ìê‡
                 {
                     Instantiate(_tilePrefab, new Vector3(length, width, 0), Quaternion.identity, transform);
-                    
+
                 }
+                if (length >= 11 && length < 12)
+                {
+                    Instantiate(_tilePrefab, new Vector3(length, width, 0), Quaternion.identity, transform);
+                }
+                
             }
         }
     }
